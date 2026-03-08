ec2af8bece9d5fc9ce0bf86ecbe531c1ffaa7b22
Yabsra Abesha <yabsraabesha165@gmail.com>
2026-03-08 11:11:02 +0300

    adding some test to the fundne.sol

--- foundry-fundme-f26/test/FundMeTest.t.sol
+++ foundry-fundme-f26/test/FundMeTest.t.sol
@@ -7,11 +7,15 @@ import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundme;
+    address USER = makeAddr("user");
+    uint256 constant SEND_VALUE = 0.1 ether;
+    uint256 constant STARTING_BALANCE = 10 ether;
 
     function setUp() external {
         // fundme = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         DeployFundMe deployfundme = new DeployFundMe();
         fundme = deployfundme.run();
+        vm.deal(USER, STARTING_BALANCE);
     }
 
     function testMinimumDollarIsFive() public view {
@@ -27,5 +31,18 @@ contract FundMeTest is Test {
     function testpricefeedisaccurate() public view {
         uint256 version = fundme.getVersion();
         assertEq(version, 4);
+    }
+
+    function testfundfailswithoutenougheth() public {
+        vm.expectRevert();
+        fundme.fund();
+    }
+
+    function testfundupdatesfundDataStructure() public {
+        vm.prank(USER); // THE NEXT TX WILL SEND BY THE USER
+
+        fundme.fund{value: SEND_VALUE}();
+        uint256 amountFunded = fundme.getAddressToAmountFunded(USER);
+        assertEq(amountFunded, SEND_VALUE);
     }
 }
