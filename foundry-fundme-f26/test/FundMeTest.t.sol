1166608fc230c0df0090359b9957f3f3017f4a15
Yabsra Abesha <yabsraabesha165@gmail.com>
2026-03-13 11:14:20 +0300

    adding some explanation and also cleaning up some codes

--- foundry-fundme-f26/test/FundMeTest.t.sol
+++ foundry-fundme-f26/test/FundMeTest.t.sol
@@ -10,6 +10,7 @@ contract FundMeTest is Test {
     address USER = makeAddr("user");
     uint256 constant SEND_VALUE = 0.1 ether;
     uint256 constant STARTING_BALANCE = 10 ether;
+    uint256 constant GAS_PRICE = 1;
 
     function setUp() external {
         // fundme = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
@@ -72,6 +73,7 @@ contract FundMeTest is Test {
         uint256 startingFundmebalance = address(fundme).balance;
 
         //Act
+
         vm.prank(fundme.getOwner());
         fundme.withdraw();
 
